2b8fe1c736f35ef68d6c63f6e5cee1883d5bd069
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-01 18:48:06 +0600

    feat: add 'Button' component to 'render' method of 'Clock' component
    - refactor 'render' method for adjustment

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import Button from './Button'
 
 class Clock extends React.Component {
     // constructor(props) {
     //     super(props);
-    //     this.state = { date: new Date() };
+    //     this.state = { date: new Date(), locale: 'bn-BD' };
+    //     this.handleClick = this.handleClick.bind(this);
     // }
 
-    state = { date: new Date() };
+    state = { date: new Date(), locale: 'bn-BD' };
 
     tick() {
         this.setState({
@@ -22,12 +24,29 @@ class Clock extends React.Component {
         clearInterval(this.clockTimer);
     }
 
+    handleClick = (locale) => {
+        this.setState({
+            locale
+        });
+    }
+
     render() {
+        const { date, locale } = this.state;
+
         return (
-            <h1 className="heading">
-                <span className="text">{this.state.date.toLocaleTimeString(this.props.locale)}</span>
-                <img src="" />
-            </h1>
+            <div>
+                <h1 className="heading">
+                    <span className="text">{this.state.date.toLocaleTimeString(locale)}</span>
+                    <img src="" />
+                </h1>
+
+                {/* <button type='button' onClick={this.handleClick}>Click here</button> */}
+                {/* <button type='button' onClick={this.handleClick.bind(this, 'en-US')}>Click here</button> */}
+                {/* <button type='button' onClick={() => this.handleClick('en-US')}>Click here</button> */}
+
+                {/* <Button change={this.handleClick} locale='en-US'></Button> */}
+                <Button change={this.handleClick} locale='en-US' />
+            </div>
         );
     }
 }
