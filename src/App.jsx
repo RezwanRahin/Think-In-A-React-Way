1e9c3d01c1243156b0ab703d3a88461a8d422a38
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-04 14:15:59 +0600

    feat: modify 'App' component and return 'ClockList' component
    - renders 3 'Clock' components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import Clock from "./components/Clock"
+import ClockList from "./components/ClockList";
 
 
 function App() {
-  return <div><Clock /></div>;
+  const quantities = [1, 2, 3];
+
+  return (
+    <div>
+      <ClockList quantities={quantities} />
+    </div>
+  );
 }
 
-export default App
+export default App;
