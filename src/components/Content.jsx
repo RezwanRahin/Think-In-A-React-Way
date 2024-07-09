44ea5406d5b6a8f24263c9708c29411cb908077d
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-09 20:24:14 +0600

    perf: introduce 'themeContext' to 'Context' component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,19 @@
 import Counter from "./Counter";
 import HoverCounter from "./HoverCounter";
+import ThemeContext from "../contexts/themeContext";
 
-function Content({ theme }) {
+function Content() {
     return (
         <div>
             <h1>This is a content</h1>
             <Counter>
-                {(counter, incrementCount) => <HoverCounter count={counter} incrementCount={incrementCount} theme={theme} />}
+                {(counter, incrementCount) => (
+                    <ThemeContext.Consumer>
+                        {({ theme }) => (
+                            <HoverCounter count={counter} incrementCount={incrementCount} theme={theme} />
+                        )}
+                    </ThemeContext.Consumer>
+                )}
             </Counter>
         </div>
     );
