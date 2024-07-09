66765898a50b76372af0d4cb610d0e8a33c44a7e
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-09 14:30:19 +0600

    feat(render props): render 'ClickCounter' & 'HoverCounter' components with 'Counter' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import ClickCounter from "./components/ClickCounter";
+import Counter from "./components/Counter";
 import HoverCounter from "./components/HoverCounter";
 
 function App() {
   return (
     <div className="app">
-      <ClickCounter />
-      <HoverCounter />
+      {/* <ClickCounter />
+      <HoverCounter /> */}
+
+      {/* <User name={() => "REZ"} /> */}
+      {/* <User render={(isLoggedIn) => isLoggedIn ? "REZ" : "Guest"} /> */}
+
+      {/* <Counter render={(counter, incrementCount) => <ClickCounter count={counter} incrementCount={incrementCount} />} />
+      <Counter render={(counter, incrementCount) => <HoverCounter count={counter} incrementCount={incrementCount} />} /> */}
+
+      <Counter>
+        {(counter, incrementCount) => <ClickCounter count={counter} incrementCount={incrementCount} />}
+      </Counter>
+      <Counter>
+        {(counter, incrementCount) => <HoverCounter count={counter} incrementCount={incrementCount} />}
+      </Counter>
     </div>
   );
 }
