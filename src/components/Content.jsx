d3586c7a2e41cfdd553c90fab13fcee00bf96e3c
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-10 15:43:12 +0600

    perf(contextType): turn the 'Content' component to a class and use 'contextType'

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,22 +1,46 @@
 import Counter from "./Counter";
 import HoverCounter from "./HoverCounter";
 import ThemeContext from "../contexts/themeContext";
+import React from "react";
 
-function Content() {
-    return (
-        <div>
-            <h1>This is a content</h1>
-            <Counter>
-                {(counter, incrementCount) => (
-                    <ThemeContext.Consumer>
-                        {({ theme, switchTheme }) => (
-                            <HoverCounter count={counter} incrementCount={incrementCount} theme={theme} switchTheme={switchTheme} />
-                        )}
-                    </ThemeContext.Consumer>
-                )}
-            </Counter>
-        </div>
-    );
+// function Content() {
+//     const context = useContext(ThemeContext);
+//     const { theme, switchTheme } = context;
+
+
+//     return (
+//         <div>
+//             <h1>This is a content</h1>
+//             <Counter>
+//                 {(counter, incrementCount) => (
+//                     <ThemeContext.Consumer>
+//                         {({ theme, switchTheme }) => (
+//                             <HoverCounter count={counter} incrementCount={incrementCount} theme={theme} switchTheme={switchTheme} />
+//                         )}
+//                     </ThemeContext.Consumer>
+//                 )}
+//             </Counter>
+//         </div>
+//     );
+// }
+
+class Content extends React.Component {
+    render() {
+        const { theme, switchTheme } = this.context;
+
+        return (
+            <div>
+                <h1>This is a content</h1>
+                <Counter>
+                    {(counter, incrementCount) => (
+                        <HoverCounter count={counter} incrementCount={incrementCount} theme={theme} switchTheme={switchTheme} />
+                    )}
+                </Counter>
+            </div>
+        );
+    }
 }
+
+Content.contextType = ThemeContext;
 
 export default Content;
