62eea1882d0ba6e3d35e495bbde846b6332a3cf5
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-13 14:58:56 +0600

    create a functional component as Todo

---
+++ src/components/Todo.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+function Todo() {
+    const [todo, setTodo] = useState('');
+    const [warning, setWarning] = useState(null);
+
+    const handleInput = (e) => {
+        const inputValue = e.target.value;
+        const updatedWarning = inputValue.includes('.js') ? 'You need JavaScript skill to complete the task. Do you have it?' : null;
+
+        setTodo(inputValue);
+        setWarning(updatedWarning);
+    }
+
+    return (
+        <div>
+            <p>{todo}</p>
+            <p>
+                <textarea name="todo" value={todo} onChange={handleInput} />
+            </p>
+            <hr />
+            <h2>{warning || 'Good Choice!'}</h2>
+        </div>
+    );
+}
+
+export default Todo;
