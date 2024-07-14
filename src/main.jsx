554e0c19da69295fae1c594ad867d5258914e9ec
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-14 14:21:40 +0600

    refactor: revert 'main' component to default code

--- src/main.jsx
+++ src/main.jsx
@@ -1,66 +1,10 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
+import App from './App.jsx'
+import './index.css'
 
-let states = [];  // [0: [value, setter], 1: [value, setter]]
-let stateIndex = -1;
-
-function useState(defaultValue) {
-  const index = ++stateIndex;
-
-  if (states[index]) {
-    return states[index];
-  }
-
-  const setValue = (newValue) => {
-    states[index][0] = newValue;
-    renderWithRezwan();
-  }
-
-  const returnArray = [defaultValue, setValue];
-  states[index] = returnArray;
-
-  return returnArray;
-}
-
-function App() {
-  const [todo, setTodo] = useState('');
-  const [warning, setWarning] = useState(null);
-
-  const handleInput = (e) => {
-    const inputValue = e.target.value;
-    const updatedWarning = inputValue.includes('.js') ? 'You need JavaScript skill to complete the task. Do you have it?' : null;
-
-    setTodo(inputValue);
-    setWarning(updatedWarning);
-  }
-
-  return (
-    <div>
-      <p>{todo}</p>
-      <p>
-        <textarea name="todo" value={todo} onChange={handleInput} />
-      </p>
-      <hr />
-      <h2>{warning || 'Good Choice!'}</h2>
-    </div>
-  );
-}
-
-const rootElement = ReactDOM.createRoot(document.getElementById('root'));
-
-function renderWithRezwan() {
-  stateIndex = -1;
-  rootElement.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  )
-}
-
-renderWithRezwan();
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//     <App />
-//   // </React.StrictMode>,
-// )
+  </React.StrictMode>,
+)
