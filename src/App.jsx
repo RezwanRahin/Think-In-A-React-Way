ef404f2af2f6dfebffae012b072cf3ef8505d7c0
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-14 16:24:27 +0600

    feat: add 'Counter' component inside 'App' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Todo from "./components/Todo";
+import Counter from "./components/Counter";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="app">
-				<Todo />
+				<Counter />
 			</div>
 		);
 	}
