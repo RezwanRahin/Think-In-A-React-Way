d29ad6530a3db51062630643cc4803cbe029358d
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-16 14:51:59 +0600

    perf: clear the interval for 'tick' method in MyComponentClass.jsx
    - add 'componentWillUnmount' method

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import MyComponent from "./components/MyComponent";
+import MyComponent from "./components/MyComponentClass";
 
 function App() {
 	const [show, setShow] = useState(true);
