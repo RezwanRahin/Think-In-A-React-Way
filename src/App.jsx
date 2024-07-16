ba7714852e6fa331e824122af441dc0fea158a47
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-16 15:19:58 +0600

    feat: add 'Title', 'ShowCount', & 'Button' component inside 'App' component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
-import MyComponent from "./components/MyComponent";
+import Title from "./components/Title";
+import ShowCount from "./components/ShowCount";
+import Button from "./components/Button";
 
 function App() {
-	const [show, setShow] = useState(true);
+	const [count1, setCount1] = useState(0);
+	const [count2, setCount2] = useState(0);
+
+	const incrementByOne = () => {
+		setCount1((prevState) => prevState + 1);
+	};
+
+	const incrementByFive = () => {
+		setCount2((prevState) => prevState + 5);
+	};
 
 	return (
 		<div className="app">
-			<div>{show && <MyComponent />}</div>
-			<p>
-				<button type="button" onClick={() => setShow((prevState) => !prevState)}>
-					{show ? 'Hide post' : 'Show post'}
-				</button>
-			</p>
+			<Title />
+			<ShowCount count={count1} title="Counter 1" />
+			<Button handleClick={incrementByOne}>Increment by one</Button>
+			<hr />
+			<ShowCount count={count2} title="Counter 2" />
+			<Button handleClick={incrementByFive}>Increment by five</Button>
 		</div>
 	);
 }
