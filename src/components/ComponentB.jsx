a4328177a2c92e7fbc5ed2c150d779865f687102
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-27 20:48:19 +0600

    feat(useReducer with useContext): create components as 'ComponentA' & 'ComponentB'

---
+++ src/components/ComponentB.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { counterContext } from "../App";
+
+function ComponentB() {
+    const countContext = useContext(counterContext);
+    return (
+        <div>
+            <p>Component B</p>
+            <button type="button" onClick={() => countContext.countDispatch('increment')}>Increment</button>
+            <button type="button" onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
+        </div>
+    );
+}
+
+export default ComponentB;
