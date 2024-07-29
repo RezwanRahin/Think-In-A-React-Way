e1785a23a7dc5a40f4286ea93c1797a2ac3c576f
rezwan <rezwanshahriorrahin@gmail.com>
2024-07-29 20:26:42 +0600

    refactor: screen size is passed as argument to 'useWindowWidth' hook in 'LayoutComponentOne' and 'LayoutComponentTwo' components

--- src/components/LayoutComponentOne.jsx
+++ src/components/LayoutComponentOne.jsx
@@ -1,7 +1,7 @@
 import useWindowWidth from './hooks/useWindowWidth';
 
 function LayoutComponentOne() {
-    const onSmallScreen = useWindowWidth();
+    const onSmallScreen = useWindowWidth(768);
 
     return (
         <div>
