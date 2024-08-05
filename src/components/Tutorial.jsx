5319fe972508dc5c2c8ad357e9237f2443909410
rezwan <rezwanshahriorrahin@gmail.com>
2024-08-05 12:51:03 +0600

    create a component as 'Tutorial'

---
+++ src/components/Tutorial.jsx
@@ -0,0 +1,12 @@
+import Container from './Container';
+import Card from './Card';
+
+function Tutorial() {
+    return (
+        <Container>
+            <Card />
+        </Container>
+    );
+}
+
+export default Tutorial;
